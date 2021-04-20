47fd7dba55b0dccf1ebe298d81cdd2814b2b74ec
kirstenmarieh <51651444+kirstenmarieh@users.noreply.github.com>
2021-04-20 19:32:49 -0400

    javadocs for favoriteslist

--- src/FavoritesList.java
+++ src/FavoritesList.java
@@ -1,12 +1,24 @@
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ * a class that constructs and manipulates a favorites list of a user.
+ */
 public class FavoritesList implements Serializable {
 
+    /**
+     * constructs a favorites list
+     * @param userID, the userid of whom to create a favorite list for.
+     */
     public FavoritesList(String userID){
         this.userid = userID;
     }
 
+    /**
+     * adds a product to the favorites list.
+     * @param p the product to add to the favorites list.
+     * @throws IOException
+     */
     public void addFave(Product p) throws IOException {
         try {
             this.getFaves();
@@ -26,6 +38,12 @@ public class FavoritesList implements Serializable {
         this.saveFaveList();
     }
 
+    /**
+     * removes a product from the favorites list.
+     * @precondition favoriteslist size > 0
+     * @param p the product to remove
+     * @throws IOException
+     */
     public void removeFave(Product p) throws IOException {
         try {
             this.getFaves();
@@ -49,6 +67,10 @@ public class FavoritesList implements Serializable {
         }
     }
 
+    /**
+     * serializes the favoriteslist
+     * @throws IOException
+     */
     public void saveFaveList() throws IOException {
         String fileName = userid +".txt";
         File file = new File(fileName);
@@ -68,6 +90,13 @@ public class FavoritesList implements Serializable {
 
     }
 
+    /**
+     * deserializes the favoriteslist.
+     * @return faves, the users favoriteslist
+     * @throws IOException
+     * @throws ClassNotFoundException
+     * @precondition: the file exists.
+     */
     public ArrayList<Product> getFaves() throws IOException, ClassNotFoundException {
         try{
             String fileName=userid+".txt";
