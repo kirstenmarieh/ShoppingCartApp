c186074a6520a4a2aa9f5d8d0a7784ff99786f46
kirstenmarieh <51651444+kirstenmarieh@users.noreply.github.com>
2021-04-20 13:35:30 -0400

    buyertest completed

---
+++ src/BuyerTest.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuyerTest {
+
+    @Test
+    void getId() {
+        System.out.println("Testing getId...");
+        Buyer buyer = new Buyer("4b", "somethingCreative", "00000");
+        Assert.assertEquals(buyer.getId(), "4b");
+    }
+
+    @Test
+    void getUsername() {
+        System.out.println("Testing getUsername...");
+        Buyer buyer = new Buyer("4b", "somethingCreative", "00000");
+        Assert.assertEquals(buyer.getUsername(), "somethingCreative");
+    }
+
+    @Test
+    void checkPassword() {
+        System.out.println("Testing checkPassword...");
+        Buyer buyer = new Buyer("4b", "somethingCreative", "00000");
+        Assert.assertTrue(buyer.checkPassword("00000"));
+    }
+}
