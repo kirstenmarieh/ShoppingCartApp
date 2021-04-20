412a7455b15be7166c81a1c01df136bcea724cd8
kirstenmarieh <51651444+kirstenmarieh@users.noreply.github.com>
2021-04-20 16:01:19 -0400

    favoritesbundle added

--- src/FavoritesBundle.java
+++ src/FavoritesBundle.java
@@ -3,50 +3,122 @@ import java.util.ArrayList;
 
 public class FavoritesBundle extends Product{
 
-    public FavoritesBundle(String name, String productID, String prodType, double salePrice,
-                           String desc, int available, double invPrice, String sellerId, String userID) throws ClassNotFoundException, IOException {
-        super(name, productID, prodType, salePrice, desc, available, invPrice, sellerId);
+    public FavoritesBundle(String userID) throws IOException, ClassNotFoundException {//String name, String productID, String prodType, double salePrice, String desc, int available, double invPrice, String sellerId, String userID
+        super();//name, productID, prodType, salePrice, desc, available, invPrice, sellerId
         this.userid = userID;
         FavoritesList faves = new FavoritesList(userid);
-        this.faveList = faves.getFaves();
-        for(Product i: faveList)
-        {
-            System.out.println("fave: " + i.getProductName());
-        }
+        faveList = faves.getFaves();
+        // for (Product p : faveList)
+        // {
+        //     (this.products).add(p);
+        // }
     }
 
     public void add(Product p)
     {
-
         products.add(p);
-        for (Product k : products)
-        {
-            System.out.println(k.getProductName());
-        }
-        System.out.println();
     }
 
     @Override
     public double getSellPrice()
     {
         double price = 0;
-        for(Product p : faveList)
+        for(Product p : products)
         {
             price += p.getSellPrice();
         }
         return price;
     }
 
-    public double getPrice(){
-        double price = 0;
-        for(Product p : faveList)
+    @Override
+    public Product getProduct() {
+        return this;
+    }
+
+    @Override
+    public int getAvailableQuantity() {
+        return 1;
+    }
+
+    @Override
+    public String getProductName() {
+        String bundleName = "";
+        for(Product p : this.products)
         {
-            price += p.getSellPrice();
+            bundleName = bundleName + " " + p.getProductName() + " ";
         }
-        return price;
+        return bundleName;
+    }
+
+    @Override
+    public void setAvailableQuantity(int quantity) {
+
+    }
+
+    @Override
+    public String getProductID(){
+        String id = "";
+        for (Product p : products)
+        {
+            id = id + " " + p.getProductID() + " ";
+        }
+        return id;
+    }
+
+    @Override
+    public String getSellerID() {
+        String sellerID = "";
+        for (Product p : products)
+        {
+            sellerID = sellerID + " " + p.getSellerID() + " ";
+        }
+        return sellerID;
+    }
+
+    @Override
+    public Double getInvoicePrice() {
+        double invPrice = 0;
+        for (Product p : products)
+        {
+            invPrice = invPrice + p.getInvoicePrice();
+        }
+        return invPrice;
+    }
+
+    @Override
+    public String getType() {
+        return "FavoritesBundle";
+    }
+
+    @Override
+    public String getProductDescription(){
+        return "FavoritesBundle";
+    }
+
+    //to be used by checkoutwindow
+    @Override
+    public void sell(int amount) {
+        for (Product p : products)
+        {
+            quantitySold+=amount;
+            availableQuantity-=amount;
+        }
+    }
+
+    @Override
+    public int getAmountSold() {
+        return 1;
+    };
+
+    @Override
+    public int getAmountInvoiced() {
+        return 1;
     }
 
     private ArrayList<Product> faveList = new ArrayList<>();
     private String userid;
     private ArrayList<Product> products = new ArrayList<>();
+    //private static final long serialVersionUID = 7058069280053434660L;
+    private int quantitySold;
+    private int availableQuantity;
 }
