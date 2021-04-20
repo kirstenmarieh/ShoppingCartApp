be884b4012e430520e75e2f62ca5557dcac19699
kirstenmarieh <51651444+kirstenmarieh@users.noreply.github.com>
2021-04-20 19:13:11 -0400

    javadoc comments added

--- src/Buyer.java
+++ src/Buyer.java
@@ -1,25 +1,47 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * a class that creates a user of type buyer.
+ */
 public class Buyer implements Serializable{
 
+    /**
+     * creates a new buyer
+     * @param idIn , the id number of the buyer.
+     * @param userIn , the username of the buyer.
+     * @param passwordIn , the password of the buyer.
+     */
     public Buyer(String idIn, String userIn, String passwordIn) {
         id=idIn;
         username=userIn;
         password=passwordIn;
     }
 
-    public  String getId() {
+    /**
+     * retrieves the id of the buyer.
+     * @return id, the id of the buyer.
+     */
+    public String getId() {
         return id;
     }
+
+    /**
+     * gets the buyers username.
+     * @return username, the username of the buyer.
+     */
     public String getUsername() {
         return username;
     }
+
+    /**
+     * validates the password of the buyer.
+     * @param input , the string to verify against the password.
+     * @return true or false, depending on validity of password.
+     */
     public boolean checkPassword(String input) {
-        System.out.println(input+password);
         return (password.equals(input));
     }
-
 
     private String id;
     private String username;
