45cd6d30a058226a64003675137804b77c5d2a37
kirstenmarieh <51651444+kirstenmarieh@users.noreply.github.com>
2021-04-20 20:35:12 -0400

    comments

--- src/SortByName.java
+++ src/SortByName.java
@@ -1,11 +1,17 @@
 import java.util.Comparator;
 
+/**
+ * a class to sort products by name in alphabetical order.
+ */
 public class SortByName implements SortStrategy {
+    /**
+     * compares the products by name
+     * @return nameSort, a comparator object
+     */
     @Override
     public Comparator<Product> productSearch() {
         Comparator<Product> nameSort= new Comparator<Product>()
         {
-
             @Override
             public int compare(Product o1, Product o2) {
                 return o1.getProductName().compareTo(o2.getProductName());
